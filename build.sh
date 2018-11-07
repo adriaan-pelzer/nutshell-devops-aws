28a5970d89bc92e3a297a60eab57c0c4a06b12b1
Adriaan Pelzer <adriaan.pelzer@gmail.com>
2018-11-07 11:26:23 +0000

    New pipeline

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+REGION="eu-west-1"
+REPONAME="new-pipeline"
+
+aws cloudformation deploy --stack-name ${REPONAME} --template-file ${REPONAME}.json --region ${REGION} --capabilities CAPABILITY_NAMED_IAM
+
+CLONEURL="$(aws codecommit get-repository --repository-name ${REPONAME} --query 'repositoryMetadata.cloneUrlSsh' --output text)"
+
+git clone ${CLONEURL}
+
+cd ${REPONAME}
+cp -rn ../repobase/* ./
+git add . && git commit -m "Repository base"
+git push origin master
+cd ..
+rm -rf ${REPONAME}
+
+echo "================================================"
+echo "Now run 'git clone ${CLONEURL}'"
+echo "================================================"
