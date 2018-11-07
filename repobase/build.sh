5fd8749392b18831eb5dd6f512b2fbf71d8f5b3b
Adriaan Pelzer <adriaan.pelzer@gmail.com>
2018-11-07 17:38:22 +0000

    Populate 3rd order repo

--- repobase/build.sh
+++ repobase/build.sh
@@ -5,3 +5,26 @@ aws s3 cp templates/* s3://${CFN_TEMPLATE_BUCKET}/${COMMIT_ID}/templates/
 
 echo "running cloudformation deployment"
 /bin/bash ./cfn-deploy.sh
+
+echo "populate repo"
+REPONAME="${PLATFORM}-${SERVICE}-repository"
+CLONEURL="$(aws codecommit get-repository --repository-name ${REPONAME} --query 'repositoryMetadata.cloneUrlSsh' --output text)"
+
+if [ "$?" == "0" ]; then
+    git clone ${CLONEURL}
+
+    cd ${REPONAME}
+    #cp -rn ../repobase/* ./
+    cp -r ../repobase/* ./
+    git add . && git commit -m "Repository base"
+    git push origin master
+    cd ..
+    rm -rf ${REPONAME}
+
+    echo "================================================"
+    echo "Now run 'git clone ${CLONEURL}'"
+    echo "================================================"
+else
+    echo "Cannot get repository url"
+    exit 1
+fi
